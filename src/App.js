b360a59655e838b0ee40bd06c89e733605050d6e
Aditya-code-collab <76222496+Aditya-code-collab@users.noreply.github.com>
2021-08-29 23:39:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,249 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
+import logo from './logo.svg';
+import './App.css';
+import cart from "./carts.png";
+function App() {
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+        <div className="App" >
+            <body >
+                <header className="App-header" >
+                    <div >
+                        <nav className="nav" >
+                            <div className="navdiv" style={{color:"rgb(20, 43, 111)"}} >
+                                <ul className="ul" >
+                                    <li className="li" > shop</li>
+                                    <li className="li" style={{fontWeight:"bold",color:"rgb(20, 43, 111)"}} > Solera </li>
+                                    <li className="li" >  <img style={{maxWidth:"30px",}} src={cart} /></li>
+                                </ul >
+                            </div>
+                        </nav >
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+                    </div>
+                </header >
+
+                <section className="pageHero22" style={{ Height: "100vh", display: "block" }}>
+                    <div id="page-hero_content" className="pageHero" >
+                        <div className="row1">
+                            <div className="pagehero2" >
+                                <h1 className="pageheroh1" >
+                                    <div className="l11" > The future </div><div style={{ textAlign: "left", fontSize: "2.8rem" }}>of health</div><div style={{ textAlign: "right" }} ><em>— is clear.</em > </div></h1 >
+                                <div class="HeroContent__ButtonsWrapper-ekwzkb-2 hYkYhy" >
+                                    <div class="HeroContent__ShopButtonWrapper-ekwzkb-3 hetKbE d-block d-sm-none" >
+                                        <a className="shopbutton"
+                                            title="Shop All"
+                                            hreflang="x-default"
+                                        >
+                                            <div class="RitualButton__StyledTextWrapper-nzwdt0-2 btVBpI" >
+                                                <span class="RitualButton__StyledText-nzwdt0-3 ediQZW HeroContent__ShopButton-ekwzkb-4 jTssZQ short alt-hover" >
+                                                    Shop All
+        </span>
+                                            </div >
+                                        </a>
+                                    </div >
+                                </div>
+                                <div class="HeroContent__ButtonsWrapper-ekwzkb-2 hYkYhy" >
+                                    <div class="HeroContent__ShopButtonWrapper-ekwzkb-3 hetKbE d-none d-sm-block" >
+                                        <a class="RitualButton__StyledLink-nzwdt0-0 kLeqxM HeroContent__ShopButton-ekwzkb-4 jTssZQ alt-hover"
+                                            title="Shop All"
+                                            hreflang="x-default"
+                                            href="/shop-all" >
+                                            <div class="RitualButton__StyledTextWrapper-nzwdt0-2 btVBpI" >
+                                                <span class="RitualButton__StyledText-nzwdt0-3 ediQZW HeroContent__ShopButton-ekwzkb-4 jTssZQ alt-hover" > Shop All
+        </span>
+                                            </div > </a> </div > </div> </div >
+                        </div>
+                    </div>
+
+                    <div data-gatsby-image-wrapper="" style={{ position: "absolute", left: "0", top: "0", width: "100%", height: "100%", zIndex: "0", userSelect: "none", userDrag: "none", pointerEvents: "none", touchCallout: "none" }} class="gatsby-image-wrapper gatsby-image-wrapper-constrained d-none d-sm-block">
+                        <div style={{ maxWidth: "1920px", display: "block" }}>
+                            <img alt="" role="presentation" aria-hidden="true" src="https://images.ctfassets.net/uuc5ok478nyh/6dDqtNr4Taao1Qwl4zf6UK/e1141217232b45bdc278746c3a6dcf87/Home-Hero-Mobile.jpg?w=912&h=912&q=90&fm=webp" style={{ maxWidth: "100%", display: "block", position: "static" }} />
+                        </div>
+                    </div>
+                </section>
+
+
+                
+
+                <div className="box02">
+
+
+                    <a class="box02div" hreflang="x-default" href="/shop-multivitamins">
+                        <div class="ProductCategoryCards__ImageContainer-sc-4filtr-4 bfytrt">
+                            <div data-gatsby-image-wrapper="" class="gatsby-image-wrapper gatsby-image-wrapper-constrained" style={{ userSelect: "none", pointerEvents: "none", backgroundColor: "rgb(255, 214, 0)" }}>
+                                <div style={{ maxWidth: "580px", display: "block", backgroundColor: "orange" }}>
+                                    <img alt="" role="presentation" aria-hidden="true" src="https://images.ctfassets.net/uuc5ok478nyh/28hfBJ1mqho10JHWUltf0X/720c1c1212efc928775e881214f528b8/Category-Multivitamin.jpg?w=580&h=359&q=90&fm=webp" style={{ maxWidth: "100%", display: "block", position: "static" }} />
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <p className="textbox2" style={{textDecoration:"none"}}>Multivitamin</p>
+                    </a>
+
+                    <a class="box02div" hreflang="x-default" href="/shop-multivitamins">
+                        <div class="ProductCategoryCards__ImageContainer-sc-4filtr-4 bfytrt">
+                            <div data-gatsby-image-wrapper="" class="gatsby-image-wrapper gatsby-image-wrapper-constrained" style={{ userSelect: "none", pointerEvents: "none", backgroundColor: "rgb(255, 214, 0)" }}>
+                                <div style={{ maxWidth: "580px", display: "block", backgroundColor: "orange" }}>
+                                    <img alt="" role="presentation" aria-hidden="true" src="https://images.ctfassets.net/uuc5ok478nyh/4HeUbLZGWaKoK7JPYdRuWg/0f36a544cda868ee9603f103c9cbefe0/Category-Protein__1_.jpg?w=580&h=359&q=90&fm=webp" style={{ maxWidth: "100%", display: "block", position: "static" }} />
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <p  className="textbox2" style={{textDecoration:"none"}}>Protein</p>
+                    </a>
+
+                    <a class="box02div" hreflang="x-default" href="/shop-multivitamins">
+                        <div class="ProductCategoryCards__ImageContainer-sc-4filtr-4 bfytrt">
+                            <div data-gatsby-image-wrapper="" class="gatsby-image-wrapper gatsby-image-wrapper-constrained" style={{ userSelect: "none", pointerEvents: "none", backgroundColor: "rgb(255, 214, 0)" }}>
+                                <div style={{ maxWidth: "580px", display: "block", backgroundColor: "orange" }}>
+                                    <img alt="" role="presentation" aria-hidden="true" src="https://images.ctfassets.net/uuc5ok478nyh/59zqn8KhaMI6DcFJ38CnDu/98d1899324e223ceec2f7d1aff84c3ee/Category-Pregnancy__1_.jpg?w=580&h=359&q=90&fm=webp" style={{ maxWidth: "100%", display: "block", position: "static" }} />
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <p  className="textbox2" style={{textDecoration:"none"}}>Pregnancy</p>
+                    </a>
+
+                    <a class="box02div" hreflang="x-default" href="/shop-multivitamins">
+                        <div class="ProductCategoryCards__ImageContainer-sc-4filtr-4 bfytrt">
+                            <div data-gatsby-image-wrapper="" class="gatsby-image-wrapper gatsby-image-wrapper-constrained" style={{ userSelect: "none", pointerEvents: "none", backgroundColor: "rgb(255, 214, 0)" }}>
+                                <div style={{ maxWidth: "580px", display: "block", backgroundColor: "orange" }}>
+                                    <img alt="" role="presentation" aria-hidden="true" src="https://images.ctfassets.net/uuc5ok478nyh/2RQ7pdzdW9JmLk0z0shhzM/cb2bdddeb740841de6153bea9e1b7d0f/Category-Bundles.jpg?w=580&h=359&q=90&fm=webp" style={{ maxWidth: "100%", display: "block", position: "static" }} />
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <p  className="textbox2" style={{textDecoration:"none"}}>Shop Bundles</p>
+                    </a>
+
+                 
+                </div>
+                
+                
+<section>
+  <div class="grid-flex">
+    <div class="col col-image imgis1" style={{backgroundImage: "url(https://images.ctfassets.net/uuc5ok478nyh/3b2rDAxg4SGASVbyiHYA4J/e1d474b1648af04c1130bc67502e7d2a/home-feat-dyson.jpg?w=1140&h=1140&q=90&fm=webp)"}}>
+    </div>
+    <div class="col col-text">
+      <div class="Aligner-item">
+          <h1 style={{textAlign:"left",color:"rgb(20, 43, 111)"}}>We're not about pseudoscience and half-truths</h1>
+        <p style={{textAlign:"left",color:"rgb(20, 43, 111)"}}>&ldquo;From Omega-3 DHA from microalgae to regeneratively-farmed pea protein, our scientists studied diets and genetics to make daily essentials based on what we need.&rdquo;
+          
+        </p>
+        <a className="shopbuttone"> Who We Are</a>
       </div>
-    )
-  }
+    </div>
+  </div>
+  
+  <div class="grid-flex">
+    <div class="col col-image imgis1" style={{backgroundImage: "url(https://images.ctfassets.net/uuc5ok478nyh/5O43D7OyK4CaCcQaaCsCAe/83c4d9f65ddef3e8ed950929e19267fc/Home-Map.jpg?w=570&h=570&q=90&fm=webp)"}}>&nbsp;
+    </div>
+    <div class="col col-text col-left">
+    <div class="Aligner-item">
+          <h1 style={{textAlign:"left",color:"rgb(20, 43, 111)"}}>We're not about pseudoscience and half-truths</h1>
+        <p style={{textAlign:"left",color:"rgb(20, 43, 111)"}}>&ldquo;From Omega-3 DHA from microalgae to regeneratively-farmed pea protein, our scientists studied diets and genetics to make daily essentials based on what we need.&rdquo;
+          
+        </p>
+        <a className="shopbuttone"> Who We Are</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="grid-flex">
+    <div class="col col-image imgis1" style={{backgroundImage: "url(https://images.ctfassets.net/uuc5ok478nyh/3Bt84vIv74GcnJ34cqoPYi/0e3739c22ea2d22dc49e375599c07dae/Home-Feat-03.jpg?w=1140&h=1140&q=90&fm=webp)"}}>&nbsp;
+    </div>
+    <div class="col col-text ">
+    <div class="Aligner-item">
+          <h1 style={{textAlign:"left",color:"rgb(20, 43, 111)"}}>We're not about pseudoscience and half-truths</h1>
+        <p style={{textAlign:"left",color:"rgb(20, 43, 111)"}}>&ldquo;From Omega-3 DHA from microalgae to regeneratively-farmed pea protein, our scientists studied diets and genetics to make daily essentials based on what we need.&rdquo;
+          
+        </p>
+        <a className="shopbuttone"> Who We Are</a>
+      </div>
+    </div>
+  </div>
+
+</section>
+             
+<hr style={{borderWidth:" 0px 0px 2px 0px",borderColor:"#142b6f",paddingTop:"50px"}}/>
+
+
+            </body>
+            <div class="footer">
+      <div class="heading">
+       
+      </div>
+      <div class="content">
+        <div class="services">
+          <h4>Shop</h4>
+          
+
+
+          <p><a href="#">Multivitamin</a></p>
+          <p><a href="#">Protein</a></p>
+          <p><a href="#">Bundles</a></p>
+          <p><a href="#">Gift Cards</a></p>
+        </div>
+        <div class="social-media">
+          <h4>About</h4>
+          
+
+
+
+
+          <p>
+            <a href="https://www.linkedin.com/in/sanket-bodke-995b5b205/"
+              ><i class="fab fa-linkedin"></i> Ingredients</a
+            >
+          </p>
+          <p>
+            <a href="https://twitter.com/Sanket46171296"
+              ><i class="fab fa-twitter"></i>Our Clinical Study</a
+            >
+          </p>
+          <p>
+            <a href="https://github.com/sanketbodke"
+              ><i class="fab fa-github"></i>The Journal</a
+            >
+          </p>
+          <p>
+            <a href="https://codepen.io/sanketbodke"
+              ><i class="fab fa-codepen"></i>Reviews</a
+            >
+          </p>
+        
+        </div>
+        <div class="links">
+          <h4>More</h4>
+          
+
+
+          <p><a href="#">Careers</a></p>
+          <p><a href="#">Press</a></p>
+          <p><a href="#">Help</a></p>
+          <p><a href="#">Contact</a></p>
+        </div>
+        
+      </div>
+      <footer>
+        <hr />
+        
+        &copy; 2021 Solera Life Science Private Limited
+      </footer>
+    </div>
+
+
+
+        </div>
+    );
 }
 
-export default App
+export default App;
+
+/*
+ <div className="box02div" style={{ backgroundColor: "green", maxWidth: "140px", maxHeight: "140px", minHeight: "140px", maxHeight: "140px" }}>
+                        <img src="" />
+
+                    </div>*/
